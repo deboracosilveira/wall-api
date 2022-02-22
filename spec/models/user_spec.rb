2b299f478ba82c65a5819984e3c09489669fe323
deboracosilveira <deboracosilveira@gmail.com>
2022-02-22 00:33:26 -0300

    test: user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(email: "john@doe.com",
+                        name: 'John',
+                        password: "john_password",
+    )
+  }
+
+  describe "Validations" do
+    it "should be valid" do
+      expect(subject).to be_valid
+    end
+
+    it "should not be valid without an email" do
+      subject.email = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "name should be less than 32 characters" do
+      subject.name = "this is a very very very long name"
+      expect(subject).to_not be_valid
+    end
+
+    it "should not be valid without a password" do
+      subject.password = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "password must be at least 6 characters" do
+      subject.password = '12'
+      expect(subject).to_not be_valid
+    end
+  end
 end

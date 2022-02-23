4747fa3e2c41fd5660672e9a5adc1443f8711890
deboracosilveira <deboracosilveira@gmail.com>
2022-02-23 19:49:42 -0300

    test: post model

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :post do
-    content { "MyText" }
-    user { nil }
+    content { FFaker::Book.description[0..255] }
+    user { build(:user) }
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe @post, type: :model do
+  before(:each) do
+    @post = FactoryBot.create(:post)
+  end
+
+  describe "Validations" do
+    it "should be valid" do
+      expect(@post).to be_valid
+    end
+
+    it "should not be valid without an user" do
+      @post.user_id = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "should not be valid without a content" do
+      @post.content = nil
+      expect(@post).to_not be_valid
+    end
+
+    it "content should not be am empty string" do
+      @post.content = "  "
+      expect(@post).to_not be_valid
+    end
+
+    it "content must be less than 300 characters" do
+      @post.content = "x" * 301
+      expect(@post).to_not be_valid
+    end
+  end
 end

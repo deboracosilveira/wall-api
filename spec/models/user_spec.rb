8e3b7248a59ebd67ddc2b4f00624e2d9c741a3a6
deboracosilveira <deboracosilveira@gmail.com>
2022-02-23 20:00:15 -0300

    fix: user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = FactoryBot.create(:user)
-
+  before(:each) do
+    @user = FactoryBot.build(:user)
+  end
+  
   describe "Validations" do
     it "should be valid" do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it "should not be valid without an email" do
-      user.email = nil
-      expect(user).to_not be_valid
+      @user.email = nil
+      expect(@user).to_not be_valid
     end
 
     it "should not be valid without a name" do
-      user.name = nil
-      expect(user).to_not be_valid
+      @user.name = nil
+      expect(@user).to_not be_valid
     end
 
     it "name should be less than 32 characters" do
-      user.name = "this is a very very very long name"
-      expect(user).to_not be_valid
+      @user.name = "this is a very very very long name"
+      expect(@user).to_not be_valid
     end
 
     it "should not be valid without a password" do
-      user.password = nil
-      expect(user).to_not be_valid
+      @user.password = nil
+      expect(@user).to_not be_valid
     end
 
     it "password must have at least 6 characters" do
-      user.password = '12'
-      expect(user).to_not be_valid
+      @user.password = '12'
+      expect(@user).to_not be_valid
     end
   end
 end

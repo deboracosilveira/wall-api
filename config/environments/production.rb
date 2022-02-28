608574d247ab29a569ab43f8edfd61325f6e765a
deboracosilveira <deboracosilveira@gmail.com>
2022-02-28 20:14:19 -0300

    feat: email settings in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,26 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "wall_api_production"
+  config.action_mailer.default_url_options = { host: 'wall-api.herokuapp.com', protocol: 'https' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'wall-api.herokuapp.com',
+    user_name:            'wallapp.noreply@gmail.com',
+    password:             'fuzkcdehqiszdfxi',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 }
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
+  
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
